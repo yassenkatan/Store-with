f405cf6771e7b20c43f823bcc6517222c21a6734
yassenkatan <yasseenkattan@gmail.com>
2022-03-02 10:27:31 +0200

    Stop Error API

--- error/api_error.js
+++ error/api_error.js
@@ -1,9 +1,9 @@
-const BaseError = require("./base_error");
+// const BaseError = require("./base_error");
 
-class APIError extends BaseError{
-    constructor(name,httpStatusCode,description,isOperational){
-        super(name,httpStatusCode,description,isOperational)
-    }
-}
+// class APIError extends BaseError{
+//     constructor(name,httpStatusCode,description,isOperational){
+//         super(name,httpStatusCode,description,isOperational)
+//     }
+// }
 
-module.exports=APIError;
+// module.exports=APIError;
--- error/error_status.js
+++ error/error_status.js
@@ -1,7 +1,7 @@
-let HttpStatusCode={
-    OK:200,
-    BAD_REQUEST:400,
-    NOT_FOUND:404,
-    INTERNAL_SERVER_ERROR:500
-}
-module.exports=HttpStatusCode;
+// let HttpStatusCode={
+//     OK:200,
+//     BAD_REQUEST:400,
+//     NOT_FOUND:404,
+//     INTERNAL_SERVER_ERROR:500
+// }
+// module.exports=HttpStatusCode;
